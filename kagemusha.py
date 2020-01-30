7d97b117f3cc279feb0362eeb18b7e18b53834d3
dqn <suyarizzz@gmail.com>
2020-01-31 03:01:02 +0900

    update: kagemusha.py

--- kagemusha.py
+++ kagemusha.py
@@ -1,63 +1,36 @@
+import pickle
 import random
-from janome.tokenizer import Tokenizer
 
-def generate_sentence(markov, head_word_pairs):
+def generate_sentence(markov, beginnings, endings):
     sentence = ''
-    (w1, w2) = random.choice(head_word_pairs)
 
+    (w1, w2) = random.choice(beginnings)
     sentence += w1 + w2
 
     while True:
         if (w1, w2) not in markov:
             return sentence
 
-        word = random.choice(markov[(w1, w2)])
-
-        if word == '\n' or word == '。':
+        if (w1, w2) in endings:
             return sentence
 
+        word = random.choice(markov[(w1, w2)])
         sentence += word
 
         w1, w2 = w2, word
 
-def make_markov(all_words):
-    markov = {}
-    w1, w2, *all_words = all_words
-
-    for word in all_words:
-        if (w1, w2) not in markov:
-            markov[(w1, w2)] = []
-
-        markov[(w1, w2)].append(word)
-        w1, w2 = w2, word
-
-    return markov
-
 def main():
-    with open('./tweets.txt') as f:
-        lines = f.readlines()
+    with open('markov.pickle', 'rb') as f:
+        data = pickle.load(f)
 
-    all_words = []
-    head_word_pairs = []
-    t = Tokenizer()
+    markov = data['markov']
+    beginnings = data['beginnings']
+    endings = data['endings']
 
-    for line in lines:
-        words = t.tokenize(line + '。', wakati = True)
+    sentences = [generate_sentence(markov, beginnings, endings) for _ in range(0, 5)]
+    best_sentence = max(sentences, key = lambda x: len(x))
 
-        if len(words) < 3:
-            continue
-
-        all_words.extend(words)
-        head_word_pairs.append((words[0], words[1]))
-
-    markov = make_markov(all_words)
-
-    sentences = []
-    for i in range(0, 5):
-        sentence = generate_sentence(markov, head_word_pairs)
-        sentences.append(sentence)
-
-    print(max(sentences, key = lambda x: len(x)))
+    print(best_sentence)
 
 if __name__ == '__main__':
     main()
